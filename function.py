9e3558c8f0e6c7318ae060169e9dc97b50e83f17
reddynavya481 <reddynavya481@gmail.com>
2020-01-08 17:10:24 +0000

    added 5 functions

--- function.py
+++ function.py
@@ -1,2 +1,61 @@
+def prime(n):
+  c=0
+  for i in range(1,n):
+    if(n%i==0):
+      c+=1
+  if(c==1):
+    return "yes"
+  else:
+    return "no"
+    
+    
 def revlist(arr):
   return arr[::-1]
+
+
+def rotating(k,ar):
+  for i in range(0,k):
+    k2=ar.pop(0)
+    ar.append(k2)
+  return ar
+
+
+def sortlists(ar1,ar2):
+  l1=ar1[:]
+  #l2=ar2[:]
+  ar1.sort()
+  #ar2.sort()
+  if(ar1==l1):
+    if(ar2==sorted(ar2)):
+      return ar1+ar2
+    else:
+      return "second array is not sorted"
+  else:
+    return "first array is not sorted"
+
+
+def numtoarr(k):
+  l=[]
+  while(k>0):
+    r=k%10
+    l.append(r)
+    k=k//10
+  return revlist(l)
+
+
+def kthlargest(ar,k):
+  ar1=ar[:]
+  ar1.sort()
+  k2=ar1[len(ar)-k]
+  ar.remove(k2)
+  return ar
+
+def mulmatrices(m1,m2):
+
+  
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 import function as f
 print(f.revlist([2,4,5,6]))
+print(f.rotating(4,[1,2,3,4,5]))
+print(f.sortlists([1,5,2],[6,7,9]))
+print(f.numtoarr(123))
+print(f.kthlargest([6,2,0,1,8,],3))
